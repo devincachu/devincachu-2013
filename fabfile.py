e9de7d49f70f08252ea3a7365914581d3566c77f
Francisco Souza <f@souza.cc>
2013-02-05 21:53:53 -0200

    fabfile: atualizando alguns comandos, cache e corrigindo erros flake8

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,7 @@ from fabric.api import cd, env, run, settings
 env.root = os.path.dirname(__file__)
 env.app = os.path.join(env.root, 'devincachu')
 env.base_dir = '/home/devincachu'
-env.project_root = os.path.join(env.base_dir, 'devincachu')
-env.app_root = os.path.join(env.project_root, 'devincachu')
+env.project_root = os.path.join(env.base_dir, 'devincachu2013')
 env.virtualenv = os.path.join(env.project_root, 'env')
 env.shell = '/bin/sh -c'
 env.hosts = ['2013.devincachu.com.br']
@@ -15,55 +14,66 @@ env.user = 'devincachu'
 
 
 def update_app():
-    run('([ -d %(project_root)s ] && cd %(project_root)s && git pull origin master) || (cd %(base_dir)s && git clone git://github.com/devincachu/devincachu.git)' % env)
+    run('([ -d %(project_root)s ] && ' +
+        'cd %(project_root)s && git fetch origin && ' +
+        'git rebase origin/master) || (cd %(base_dir)s && ' +
+        'git clone git://github.com/devincachu/devincachu-2013.git ' +
+        '%(project_root)s)' % env)
 
 
 def create_virtualenv_if_need():
-    run('[ -d %(virtualenv)s ] || virtualenv --no-site-packages --unzip-setuptools %(virtualenv)s' % env)
+    run('[ -d %(virtualenv)s ] || virtualenv %(virtualenv)s' % env)
 
 
 def pip_install():
-    run('CFLAGS=-I/usr/local/include LDFLAGS=-L/usr/local/lib %(virtualenv)s/bin/pip install -r %(project_root)s/requirements_env.txt' % env)
+    run('CFLAGS=-I/usr/local/include LDFLAGS=-L/usr/local/lib ' +
+        '%(virtualenv)s/bin/pip install -r ' +
+        '%(project_root)s/requirements_env.txt' % env)
 
 
 def collect_static_files():
-    with cd(env.app_root):
-        run('%(virtualenv)s/bin/python manage.py collectstatic -v 0 --noinput' % env)
+    with cd(env.project_root):
+        run('%(virtualenv)s/bin/python manage.py collectstatic ' +
+            '-v 0 --noinput' % env)
 
 
 def syncdb():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run('%(virtualenv)s/bin/python manage.py syncdb --noinput' % env)
 
 
 def start_gunicorn():
-    with cd(env.app_root):
-        run('%(virtualenv)s/bin/gunicorn --pid=gunicorn.pid --daemon --workers=3 --access-logfile=devincachu_access.log --error-logfile=devincachu_error.log --bind=127.0.0.1:8989 devincachu.wsgi' % env)
+    with cd(env.project_root):
+        run('%(virtualenv)s/bin/gunicorn --pid=gunicorn.pid --daemon ' +
+            '--workers=3 --access-logfile=devincachu_access.log ' +
+            '--error-logfile=devincachu_error.log --bind=127.0.0.1:8989 ' +
+            'devincachu.wsgi' % env)
 
 
 def stop_gunicorn():
     with settings(warn_only=True):
-        pid = run('cat %(app_root)s/gunicorn.pid' % env)
+        pid = run('cat %(project_root)s/gunicorn.pid' % env)
         run('kill -TERM %s' % pid)
 
 
 def graceful_gunicorn():
     with settings(warn_only=True):
-        pid = run('cat %(app_root)s/gunicorn.pid' % env)
+        pid = run('cat %(project_root)s/gunicorn.pid' % env)
         run('kill -HUP %s' % pid)
 
 
 def restart_nginx():
-    run("su -m root -c '/usr/local/sbin/nginx -s stop && /usr/local/sbin/nginx'")
+    run("su -m root -c '/usr/local/etc/rc.d/nginx restart'")
 
 
 def clean():
-    run("su -m root -c 'rm -rf /usr/local/etc/nginx/cache/data/devincachu/*'")
+    run("su -m root -c " +
+        "'rm -rf /usr/local/etc/nginx/cache/data/devincachu2013/*'")
     restart_nginx()
 
 
 def createsuperuser():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run("%(virtualenv)s/bin/python manage.py createsuperuser" % env)
 
 
