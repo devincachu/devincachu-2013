a9779bd490583059d4d51023513dd155936fba2b
Francisco Souza <f@souza.cc>
2013-02-12 12:59:33 -0200

    urls: adicionando página "quando e onde"
    Esse ano não vai ser flat page :-)

--- devincachu/urls.py
+++ devincachu/urls.py
@@ -5,6 +5,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import include, patterns, url
 from django.views.decorators import csrf
+from django.views.generic import base
 
 from django.contrib import admin
 admin.autodiscover()
@@ -17,38 +18,43 @@ from purger import connect
 connect()
 
 p = patterns
-urlpatterns = p('',
-                url(r'^admin/', include(admin.site.urls)),
-                url(r'^palestrantes/$',
+urlpatterns = p("",
+                url(r"^admin/", include(admin.site.urls)),
+                url(r"^palestrantes/$",
                     pviews.PalestrantesView.as_view(),
-                    name='palestrantes'),
-                url(r'^programacao/$',
+                    name="palestrantes"),
+                url(r"^programacao/$",
                     pviews.ProgramacaoView.as_view(),
-                    name='programacao'),
-                url(r'^programacao/(?P<palestrantes>.*)/(?P<slug>[\w-]+)/$',
+                    name="programacao"),
+                url(r"^programacao/(?P<palestrantes>.*)/(?P<slug>[\w-]+)/$",
                     pviews.PalestraView.as_view(),
-                    name='palestra'),
-                url(r'^inscricao/$',
+                    name="palestra"),
+                url(r"^inscricao/$",
                     iviews.Inscricao.as_view(),
-                    name='inscricao'),
-                url(r'^notificacao/$',
+                    name="inscricao"),
+                url(r"^notificacao/$",
                     csrf.csrf_exempt(iviews.Notificacao.as_view()),
-                    name='notificacao'),
-                url(r'^certificado/validar/$',
+                    name="notificacao"),
+                url(r"^certificado/validar/$",
                     iviews.ValidacaoCertificado.as_view(),
-                    name='validacao_certificado'),
-                url(r'^certificado/$',
+                    name="validacao_certificado"),
+                url(r"^certificado/$",
                     iviews.BuscarCertificado.as_view(),
-                    name='busca_certificado'),
-                url(r'^certificado/(?P<slug>[0-9a-f]+)/$',
+                    name="busca_certificado"),
+                url(r"^certificado/(?P<slug>[0-9a-f]+)/$",
                     iviews.Certificado.as_view(),
-                    name='certificado'),
-                url(r'^$', dviews.IndexView.as_view(), name='index'),
+                    name="certificado"),
+                url(r"^quando-e-onde/$",
+                    base.TemplateView.as_view(
+                        template_name="quando-e-onde.html",
+                    ),
+                    name="quando-e-onde"),
+                url(r"^$", dviews.IndexView.as_view(), name="index"),
                 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-                            url(r'^media/(?P<path>.*)$',
-                            'django.views.static.serve',
-                            {'document_root': settings.MEDIA_ROOT}),
+    urlpatterns += patterns("",
+                            url(r"^media/(?P<path>.*)$",
+                            "django.views.static.serve",
+                            {"document_root": settings.MEDIA_ROOT}),
                             )
